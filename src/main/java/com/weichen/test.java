50aed3077868f77eaa54e1aa95b8700385d5e33d
weichen <519543138@qq.com>
2023-03-13 21:24:52 +0800

    first commit

---
+++ src/main/java/com/weichen/test.java
@@ -0,0 +1,12 @@
+package com.weichen;
+
+/**
+ * @author weichen
+ * @email 519543138@qq.com
+ * @date 2023-03-13 下午9:22
+ */
+public class test {
+    public static void main(String[] args) {
+
+    }
+}
